1237861cbed0eef51f2bd807051e3c0dc5e6221d
mossv2 <mossv2@github.com>
2025-02-25 09:58:14 +0800

    add membase hub upload

--- python/packages/autogen-core/src/autogen_core/model_context/_chat_completion_context.py
+++ python/packages/autogen-core/src/autogen_core/model_context/_chat_completion_context.py
@@ -6,6 +6,17 @@ from pydantic import BaseModel, Field
 from .._component_config import ComponentBase
 from ..models import LLMMessage
 
+import json
+import uuid
+from aip_hub.hub import hub_client
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv() 
+
+membase_account = os.getenv('MEMBASE_ACCOUNT')
+
 
 class ChatCompletionContext(ABC, ComponentBase[BaseModel]):
     """An abstract base class for defining the interface of a chat completion context.
@@ -20,17 +31,38 @@ class ChatCompletionContext(ABC, ComponentBase[BaseModel]):
 
     def __init__(self, initial_messages: List[LLMMessage] | None = None) -> None:
         self._messages: List[LLMMessage] = initial_messages or []
+        self._conversation_id = str(uuid.uuid4())
+        if membase_account and membase_account != "":
+            self._owner = membase_account
+        else: 
+            self._owner = self._conversation_id 
+        self._message_id = 0
 
     async def add_message(self, message: LLMMessage) -> None:
         """Add a message to the context."""
         self._messages.append(message)
 
+        msg = message.model_dump()
+        memory_id = self._conversation_id + "_" + str(self._message_id)
+        self._message_id += 1
+        msg['conversation'] = self._conversation_id
+        msg['owner'] = self._owner
+        try:
+            msgdict = json.dumps(msg, ensure_ascii=False)
+        except Exception as e:
+            msgdict = json.dumps(msg)
+
+        print(f"Upload memory: {self._owner} {memory_id}")
+        hub_client.upload_hub(self._owner, memory_id, msgdict)
+     
     @abstractmethod
     async def get_messages(self) -> List[LLMMessage]: ...
 
     async def clear(self) -> None:
         """Clear the context."""
         self._messages = []
+        self._conversation_id = str(uuid.uuid4())
+        self._message_id = 0
 
     async def save_state(self) -> Mapping[str, Any]:
         return ChatCompletionContextState(messages=self._messages).model_dump()
